ee3f3eca0f6da1108f3079de3dd9553c28782854
Shane Kaestner <shane.kaestner@yahoo.com>
2022-08-17 23:42:26 -0700

    added some more routing options for Next as well as a basic 404 page.

---
+++ pages/404.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function NotFoundPage() {
+  return (
+    <div>
+      <f1>Page Not Found</f1>
+    </div>
+  );
+}
--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -1,13 +1,22 @@
 import React from "react";
 import { useRouter } from "next/router";
 
-export default function ClientProject() {
+export default function ClientProjectPage() {
   const router = useRouter();
 
   console.log(router.query);
+
+  function loadProjectHandler() {
+    // business logic
+    router.push({
+      pathname: "/clients/[id]/[clientprojectid",
+      query: { id: "max", clientprojectid: "projecta" },
+    });
+  }
   return (
     <div>
       <h1>Client Project</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 }
